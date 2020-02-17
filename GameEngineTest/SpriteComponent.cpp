6fc2a449d9894252f062efa3d64e45d160329d8d
Ivan M <massyutin@gmail.com>
2020-02-17 16:58:06 +0300

    New Sprite constructor to use animation parameters

--- GameEngineTest/SpriteComponent.cpp
+++ GameEngineTest/SpriteComponent.cpp
@@ -2,7 +2,51 @@
 
 SpriteComponent::SpriteComponent(const char* filePath)
 {
+	isAnimated = false;
+	isFixed = false;
 	SetTexture(filePath);
+}
+
+SpriteComponent::SpriteComponent(std::string id, int numFrames, int animSpeed, bool hasDirections, bool isFixed)
+	:
+	numFrames(numFrames),
+	animationSpeed(animSpeed),
+	isFixed(isFixed)
+{
+	isAnimated = true;
+
+	if (hasDirections)
+	{
+		Animation downAnimation = Animation(0, numFrames, animationSpeed);
+		Animation rightAnimation = Animation(1, numFrames, animationSpeed);
+		Animation leftAnimation = Animation(2, numFrames, animationSpeed);
+		Animation upAnimation = Animation(3, numFrames, animationSpeed);
+
+		animations.emplace("DownAnimation", downAnimation);
+		animations.emplace("RightAnimation", rightAnimation);
+		animations.emplace("LeftAnimation", leftAnimation);
+		animations.emplace("UpAnimation", upAnimation);
+
+		this->animIndex = 0;
+		this->curAnimName = "DownAnimation";
+	}
+	else
+	{
+		Animation singleAnimation = Animation(0, numFrames, animationSpeed);
+		animations.emplace("SingleAnimation", singleAnimation);
+		this->animIndex = 0;
+		this->curAnimName = "SingleAnimation";
+	}
+	Play(this->curAnimName);
+	SetTexture(id);
+}
+
+void SpriteComponent::Play(std::string animationName)
+{
+	numFrames = animations[animationName].numFrames;
+	animIndex = animations[animationName].index;
+	animationSpeed = animations[animationName].animSpeed;
+	curAnimName = animationName;
 }
 
 void SpriteComponent::SetTexture(std::string assetTextureId)
@@ -21,8 +65,13 @@ void SpriteComponent::Initialize()
 
 void SpriteComponent::Update(float dt)
 {
-	destRect.x = (int)transform->pos.x;
-	destRect.y = (int)transform->pos.y;
+	if (isAnimated)
+	{
+		sourceRect.x = sourceRect.w * static_cast<int>((SDL_GetTicks() / animationSpeed) % numFrames);
+	}
+	sourceRect.y = animIndex * transform->height;
+	destRect.x = static_cast<int>(transform->pos.x);
+	destRect.y = static_cast<int>(transform->pos.y);
 	destRect.w = transform->width * transform->scale;
 	destRect.h = transform->height * transform->scale;
 }
--- GameEngineTest/SpriteComponent.h
+++ GameEngineTest/SpriteComponent.h
@@ -3,6 +3,7 @@
 #include "TextureManager.h"
 #include "AssetManager.h"
 #include "TransformComponent.h"
+#include "Animation.h"
 
 class SpriteComponent : public Component
 {
@@ -11,9 +12,18 @@ private:
 	SDL_Texture* texture;
 	SDL_Rect sourceRect;
 	SDL_Rect destRect;
+	bool isAnimated;
+	int numFrames;
+	int animationSpeed;
+	bool isFixed;
+	std::map<std::string, Animation> animations;
+	std::string curAnimName;
+	unsigned int animIndex = 0;
 public:
 	SDL_RendererFlip spriteFlip = SDL_FLIP_NONE;
 	SpriteComponent(const char* filePath);
+	SpriteComponent(std::string id, int numFrames, int animSpeed, bool hasDirections, bool isFixed);
+	void Play(std::string animationName);
 	void SetTexture(std::string assetTextureId);
 	void Initialize() override;
 	void Update(float dt) override;
